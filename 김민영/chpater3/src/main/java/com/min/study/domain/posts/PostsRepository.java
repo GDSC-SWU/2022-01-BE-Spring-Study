541d8e6cfd3fe62105f1ca83906101a72c12881b
Skyminn <rucy0716@naver.com>
2022-11-07 23:02:19 +0900

    chapter 03.스프링 부트에서 JPA로 데이터베이스 다뤄보자

---
+++ 김민영/chpater3/src/main/java/com/min/study/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.min.study.domain.posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+}
---
+++ 김민영/chpater3/src/main/java/com/min/study/web/PostsApiController.java
@@ -0,0 +1,29 @@
+package com.min.study.web;
+
+import com.min.study.service.posts.PostsService;
+import com.min.study.web.dto.PostsResponseDto;
+import com.min.study.web.dto.PostsSaveRequestDto;
+import com.min.study.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts") //Post 방식(등록)
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}") //Put 방식(수정)
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}") //Get 방식(조회)
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
