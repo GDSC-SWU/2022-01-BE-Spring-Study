58266ca4cc84c24dbc452592abd8bbaad71d4e5d
Skyminn <rucy0716@naver.com>
2022-11-13 15:54:21 +0900

    chpater 04.머스테치로 화면 구성하기

--- 김민영/spring/src/main/java/com/min/study/service/posts/PostsService.java
+++ 김민영/spring/src/main/java/com/min/study/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.min.study.service.posts;
 
 import com.min.study.domain.posts.Posts;
 import com.min.study.domain.posts.PostsRepository;
+import com.min.study.web.dto.PostsListResponseDto;
 import com.min.study.web.dto.PostsResponseDto;
 import com.min.study.web.dto.PostsSaveRequestDto;
 import com.min.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,8 +32,25 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
